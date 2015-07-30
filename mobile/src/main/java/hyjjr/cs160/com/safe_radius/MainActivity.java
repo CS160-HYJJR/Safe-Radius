50daaba97ba42f30807c58a2ff9da2995b4701cc
Harry He <hehaoqian@live.com>
2015-07-29 19:24:39 -0700

    Interface update

--- mobile/src/main/java/hyjjr/cs160/com/safe_radius/MainActivity.java
+++ mobile/src/main/java/hyjjr/cs160/com/safe_radius/MainActivity.java
@@ -3,7 +3,6 @@ package hyjjr.cs160.com.safe_radius;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.TabHost;
 
 public class MainActivity extends TabActivity {
@@ -16,27 +15,12 @@ public class MainActivity extends TabActivity {
 
         TabHost.TabSpec tab1 = tabHost.newTabSpec(getString(R.string.tab1_name))
                 .setIndicator(getString(R.string.tab1_name))
-                .setContent(new Intent(this, RadarActivity.class));
+                .setContent(new Intent(this, SendActivity.class));
         TabHost.TabSpec tab2 = tabHost.newTabSpec(getString(R.string.tab2_name))
                 .setIndicator(getString(R.string.tab2_name))
                 .setContent(new Intent(this, RadarActivity.class));
 
         tabHost.addTab(tab1);
         tabHost.addTab(tab2);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- mobile/src/main/java/hyjjr/cs160/com/safe_radius/SendActivity.java
+++ mobile/src/main/java/hyjjr/cs160/com/safe_radius/SendActivity.java
@@ -1,30 +1,14 @@
 package hyjjr.cs160.com.safe_radius;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.MenuItem;
 
 
-public class SendActivity extends ActionBarActivity {
+public class SendActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
