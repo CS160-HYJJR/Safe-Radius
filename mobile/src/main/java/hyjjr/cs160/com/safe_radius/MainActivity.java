294a4e593ff630172806407a1c7a06bdf3b7f71d
Harry He <hehaoqian@live.com>
2015-07-31 16:14:14 -0700

    update

--- mobile/src/main/java/hyjjr/cs160/com/safe_radius/MainActivity.java
+++ mobile/src/main/java/hyjjr/cs160/com/safe_radius/MainActivity.java
@@ -3,7 +3,6 @@ package hyjjr.cs160.com.safe_radius;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
-import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
 
@@ -18,19 +17,9 @@ public class MainActivity extends FragmentActivity {
         mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
 
-        TextView view1 = new TextView(this);
-        /*
-        view1.setText(getString(R.string.tab1_name));
-        view1.setHeight(20);
-        view1.setWidth(50);*/
         mTabHost.addTab(mTabHost.newTabSpec(getString(R.string.tab1_name)).setIndicator(getString(R.string.tab1_name)),
                 SendFragment.class, null);
 
-        TextView view2 = new TextView(this);
-        /*
-        view2.setText(getString(R.string.tab2_name));
-        view2.setHeight(20);
-        view2.setWidth(50);*/
         mTabHost.addTab(mTabHost.newTabSpec(getString(R.string.tab2_name)).setIndicator(getString(R.string.tab2_name)),
                 RadarFragment.class, null);
     }
