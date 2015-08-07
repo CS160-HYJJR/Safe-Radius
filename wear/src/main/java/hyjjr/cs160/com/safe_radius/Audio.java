3afcad552885c2494bab0c4c461ce475d3008594
Harry He <hehaoqian@live.com>
2015-08-07 00:17:52 -0700

    experimental mic on watch. Won't work on emulator

---
+++ wear/src/main/java/hyjjr/cs160/com/safe_radius/Audio.java
@@ -0,0 +1,81 @@
+/*
+@author: stackoverflow/ Rahul Baradia
+ */
+
+package hyjjr.cs160.com.safe_radius;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+import java.io.ByteArrayOutputStream;
+
+public class Audio {
+    private static final int RECORDER_SAMPLERATE = 8000;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord recorder = null;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
+    int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+    int BytesPerElement = 2; // 2 bytes in 16bit format
+    short sData[] = new short[BufferElements2Rec];
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+    public void startRecording() {
+
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            public void run() {
+                writeAudio();
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+    }
+
+    //convert short to byte
+    private byte[] short2byte(short[] sData) {
+        int shortArrsize = sData.length;
+        byte[] bytes = new byte[shortArrsize * 2];
+        for (int i = 0; i < shortArrsize; i++) {
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+            sData[i] = 0;
+        }
+        return bytes;
+
+    }
+
+    private void writeAudio() {
+        // Write the output audio in byte
+        short sData[] = new short[BufferElements2Rec];
+
+        while (isRecording) {
+            // gets the voice output from microphone to byte format
+            recorder.read(sData, 0, BufferElements2Rec);
+                // // writes the data to file from buffer
+                // // stores the voice buffer
+            byte bData[] = short2byte(sData);
+            os.write(bData, 0, BufferElements2Rec * BytesPerElement);
+        }
+    }
+
+    public byte[] stopRecording() {
+        // stops the recording activity
+        isRecording = false;
+        if (null != recorder) {
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            recordingThread = null;
+            return os.toByteArray();
+        }
+        return new byte[0];
+    }
+
+}
