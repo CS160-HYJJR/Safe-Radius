37a63326d7817358eff43609891b30889dc2ed4a
Harry He <hehaoqian@live.com>
2015-08-07 21:48:57 -0700

    change location update freq

--- mobile/src/main/java/hyjjr/cs160/com/safe_radius/RadarFragment.java
+++ mobile/src/main/java/hyjjr/cs160/com/safe_radius/RadarFragment.java
@@ -24,8 +24,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class RadarFragment extends Fragment implements OnMapReadyCallback {
 
     private static final String TAG = RadarFragment.class.getSimpleName();
-    private static int UPDATE_INTERVAL_MS = 3000;
-    private static int FASTEST_INTERVAL_MS = 1500;
+    private static int UPDATE_INTERVAL_MS = 4000;
+    private static int FASTEST_INTERVAL_MS = 2000;
     private static final int ZOOM_LEVEL = 19;
 
     private static View view;
